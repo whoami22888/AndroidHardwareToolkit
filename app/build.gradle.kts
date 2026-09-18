6eba1a595fe8208c808d99175b6729a864c7092f
Whoami <132847218+whoami22888@users.noreply.github.com>
2026-09-18 18:46:58 +1000

    Align app version with 0.9 release UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.android.hardwaretoolkit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 80
-        versionName = "0.8.0"
+        versionCode = 90
+        versionName = "0.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
